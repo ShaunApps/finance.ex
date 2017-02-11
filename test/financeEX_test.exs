eb21c648f6596f1286fce531cf0ad44779f0f6c5
ShaunApps <shaun.apps@gmail.com>
2017-02-10 21:19:00 -0500

    tests failing, might be how elixir handles numbers vs js

--- test/financeEX_test.exs
+++ test/financeEX_test.exs
@@ -4,8 +4,12 @@ defmodule FinanceEXTest do
 
   import FinanceEX
 
-  test "AM returns the correct value" do
+  test "AM returns the correct value when payment type is years and paysatbeginning" do
     assert am(20000, 7.5, 5, 0, 1) == 400.76
+  end
+
+  test "AM returns the correct value when payment type is months and paysatbeginning" do
+    assert am(20000, 7.5, 5, 1, 1) == 400.76
   end
 
   # test "the truth" do
